2b176c5999166d5e626953682223ba54eddba535
Dmitriy Snyatkov <d-snyatkov@mail.ru>
2023-02-15 00:42:04 +0300

    Added validation to register page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 class UserController extends Controller
 {
     public function register(Request $request)
     {
         $user = new User();
-        $user->email = $request->input('email');
-        $user->password = $request->input('password');
+        $request->validate([
+            'email' => 'required|email',
+            'password' =>  ['required', Password::min(8)->letters()->mixedCase()->numbers()],
+        ]);
+        $input = $request->all();
+
+        $user->email = $input['email'];
+        $user->password = Hash::make($input['password']);
         $user->save();
-        return redirect('/');
+
+        return redirect('/'); //Need to add success message
+    }
+    public function login(Request $request)
+    {
+        //TODO complete the login method
+        $userData = User::where('email','=',  $request->input('email'))->get();
+        //return redirect('/');
     }
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
-                                <h1>Register</h1>
+                                            <h1>Register</h1>
+    <h1>Password must have at least 8 characters, Uppercase letter and number</h1>
 <form action="{{url('registerForm')}}" method="POST">
     @csrf
 <label class="">
@@ -14,4 +15,13 @@
 
     <button type="submit" value= "submit" > Submit</button>
 </form>
+    @if ($errors->any())
+        <div class="">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </x-layout>
