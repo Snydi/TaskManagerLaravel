37eee88d2d8b0f4ee51a21309a0de34bb220c5f1
snydi <d-snyatkov@mail.ru>
2023-03-02 22:14:31 +0300

    CSS improvements

--- resources/views/task/readTask.blade.php
+++ resources/views/task/readTask.blade.php
@@ -4,29 +4,32 @@
                                     <h1 class="centered__text">Tasks</h1>
 
     <div class="content__wrapper">
-        <a href="/createTask"> <button class="button">Create task</button></a>
+        <a href="/createTask"> <button class="button button__create">Create task</button></a>
+
     <table class="table " >
         <thead>
         <tr>
-            <th>Task</th>
-            <th>Status</th>
-            <th>Deadline</th>
-            <th>Complete</th>
-            <th>Update</th>
-            <th>Delete</th>
+            <th class="main__table">Task</th>
+            <th class="main__table">Status</th>
+            <th class="main__table">Deadline</th>
+            <th class="table__buttons">Actions</th>
         </tr>
         </thead>
         <tbody>
+
     @foreach ($tasks as $task)
         <tr>
             <td class="centered__text">{{ $task->task }}</td>
             <td class="centered__text">{{ $task->status }}</td>
             <td class="centered__text">{{ $task->deadline }}</td>
-            <td> <a href="/completeTask"> <button class="button"> Complete task</button></a> </td>
-            <td> <a href="/updateTask/{{$task->id}}"> <button class="button"> Update task</button></a> </td>
-            <td> <a href="/deleteTask/{{$task->id}}"> <button class="button"> Delete task</button></a> </td>
+            <td class="table__buttons">
+                <a href="/completeTask"> <button class="button button__table"> Complete</button></a>
+                <a href="/updateTask/{{$task->id}}"> <button class="button button__table"> Update</button></a>
+                <a href="/deleteTask/{{$task->id}}"> <button class="button button__table"> Delete</button></a>
+            </td>
         </tr>
     @endforeach
+
         </tbody>
     </table>
     </div>
