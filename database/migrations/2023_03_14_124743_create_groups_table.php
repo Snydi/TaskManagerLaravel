c07aa9ae6c1c0a4cba9c06a708bb74d4cbe4180c
Snydi <90327066+Snydi@users.noreply.github.com>
2023-04-11 22:35:44 +0300

    Fixed foreign key constraint bug

--- database/migrations/2023_03_14_124743_create_groups_table.php
+++ database/migrations/2023_03_14_124743_create_groups_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->text('group');
-            $table->unsignedBigInteger('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('groupes');
+        Schema::dropIfExists('groups');
     }
 };
